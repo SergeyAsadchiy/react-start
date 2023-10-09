4da7139b9c92295d33709434ac17da73aaf3bd8a
Sergey Asadchiy <sergey.asadchiy@eltrino.nl>
2023-10-09 21:27:33 +0300

    counter component

---
+++ src/components/counter.jsx
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+
+const Counter = () => {
+  const [count, setCount] = useState(0)
+
+  function increment () {
+    setCount(count+1)
+  }
+  function decrement () {
+    setCount(count-1)
+  }
+
+  return (
+    <div>
+      <h1> {count}</h1>
+      <input
+        type="text"
+        value={count}
+        onChange={event => setCount(event.target.value)}
+      />
+      <button onClick={increment}> + </button>
+      <button onClick={decrement}> - </button>
+    </div>
+  );
+};
+
+export default Counter;
