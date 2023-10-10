280ba26025caa91edea144e74511d95000637cbb
Sergey Asadchiy <sergey.asadchiy@eltrino.nl>
2023-10-10 11:51:41 +0300

    postList component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, {useState} from "react";
-import PostItem from "./components/PostItem";
 import './styles/App.css'
+import PostList from "./components/PostList";
 
 function App() {
   const [posts] = useState([
-    {id: 1, title: 'Javascript', text: 'description'},
-    {id: 2, title: 'Javascript', text: 'description'},
-    {id: 3, title: 'Javascript', text: 'description'}
+    {id: 1, title: 'Javascript 1', text: 'description'},
+    {id: 2, title: 'Javascript 2', text: 'description'},
+    {id: 3, title: 'Javascript 3', text: 'description'}
+  ])
+  const [posts2] = useState([
+    {id: 1, title: 'Python 1', text: 'description'},
+    {id: 2, title: 'Python 2', text: 'description'},
+    {id: 3, title: 'Python 3', text: 'description'}
   ])
   return (
     <div className="App">
-      <h1 style={{marginTop: '20px', textAlign: 'center'}}>Список постов</h1>
-      {
-        posts.map(post => <PostItem post={post} key={post.id}/>)
-      }
+      <PostList posts={posts} title={'Список постов по JS'} />
+      <PostList posts={posts2} title={'Список постов по Python'} />
     </div>
   );
 }
