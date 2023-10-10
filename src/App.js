c36e30b16c87b5a8a427576ac487c0df6a82a45d
Sergey Asadchiy <sergey.asadchiy@eltrino.nl>
2023-10-10 13:07:47 +0300

    form: controlled component

--- src/App.js
+++ src/App.js
@@ -15,12 +15,25 @@ function App() {
     {id: 2, title: 'Python 2', text: 'description'},
     {id: 3, title: 'Python 3', text: 'description'}
   ])
+
+  const [title, setTitle] = useState('default title')
+
+  const addNewPost = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <div className="App">
       <form action="">
-        <MyInput type="text" placeholder="Название поста" />
+        {/*управляемій компонет*/}
+        <MyInput
+          type="text"
+          placeholder="Название поста"
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+        />
         <MyInput type="text" placeholder="Описание поста" />
-        <MyButton disabled>Добавить пост</MyButton>
+        <MyButton onClick={addNewPost}>Добавить пост</MyButton>
       </form>
       <PostList posts={posts} title={'Список постов по JS'} />
       <PostList posts={posts2} title={'Список постов по Python'} />
