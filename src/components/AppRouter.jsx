9b522d66cf85ee35d0c40e137050e0956c0bff23
Sergey Asadchiy <sergey.asadchiy@eltrino.nl>
2023-10-16 22:31:17 +0300

    login page and private routes

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -4,22 +4,35 @@ import About from "../pages/About";
 import Posts from "../pages/Posts";
 import Error from "../pages/Error";
 import PostPage from "../pages/PostPage";
+import Login from "../pages/Login";
 
-const routes = [
+const privateRoutes = [
   {path: '/about', element: <About/>},
   {path: '/posts', element: <Posts/>},
   {path: '/posts/:id', element: <PostPage/>},
   {path: '/', element: <Posts/>},
   {path: '*', element: <Error/>},
-
 ]
+
+const publicRoutes = [
+  {path: '/login', element: <Login/>},
+  {path: '/', element: <Login/>},
+  {path: '*', element: <Error/>},
+]
+
+const isAuth = false
 
 const AppRouter = () => {
   return (
     <Routes>
-      {routes.map(route =>
-        <Route path={route.path} element={route.element}/>
-      )}
+      {isAuth
+        ? privateRoutes.map(route =>
+          <Route path={route.path} element={route.element} key={route.path}/>
+        )
+        :publicRoutes.map(route =>
+          <Route path={route.path} element={route.element} key={route.path}/>
+        )
+      }
     </Routes>
   );
 };
