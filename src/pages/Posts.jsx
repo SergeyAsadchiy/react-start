71f392f6167587d2e95dc1db7a53ed25b5118bf4
Sergey Asadchiy <sergey.asadchiy@eltrino.nl>
2023-10-17 18:38:57 +0300

    26_dynamic_paginatin_on_page_scroll

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import PostList from "../components/PostList";
 import PostForm from "../components/PostForm";
 import PostFilter from "../components/PostFilter";
@@ -10,28 +10,35 @@ import Loader from "../components/UI/Loader/Loader";
 import {useFetching} from "../hooks/useFetching";
 import {getPagesCount} from "../utils/pages";
 import Pagination from "../components/UI/pagination/Pagination";
+import {useObserver} from "../hooks/useObserver";
+import MySelect from "../components/UI/select/MySelect";
 
 function Posts() {
   const [posts, setPosts] = useState([])
   const [filter, setFilter] = useState({sort: '', query: ''})
   const [visibleModal, setVisibleModal] = useState(false)
-  const [limit] = useState(5)
+  const [limit, setLimit] = useState(10)
   const [page, setPage] = useState(1)
   const [totalPages, setTotalPages] = useState(0)
+  const lastElement = useRef()
 
   const [fetchPosts, isPostsLoading, postsError] = useFetching(async (limit, page) => {
     const response = await PostsService.getAll(limit, page)
-    setPosts(response.data)
+    setPosts([...posts, ...response.data])
     const totalCount = response.headers['x-total-count']
     setTotalPages(getPagesCount(totalCount, limit))
   })
 
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
+  useObserver(lastElement, page < totalPages, isPostsLoading, () => {
+    setPage(page + 1)
+  })
+
   useEffect(() => {
     fetchPosts(limit, page)
     // eslint-disable-next-line
-  }, [])
+  }, [page, limit])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -60,19 +67,34 @@ function Posts() {
         filter={filter}
         setFilter={setFilter}
       />
+      <MySelect
+        value={limit}
+        onChange={e => setLimit(e)}
+        defaultValue="Кол-во постов на странице"
+        options={[
+          {value: 5, name: '5'},
+          {value: 10, name: '10'},
+          {value: 20, name: '20'},
+          {value: -1, name: 'Все посты'}
+        ]}
+      />
       {postsError &&
       <h3>Произошла ошибка: {postsError}</h3>}
-      {isPostsLoading
-        ? <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}>
+      {posts.length}
+      <PostList
+        remove={removePost}
+        posts={sortedAndSearchedPosts}
+        title={'Список постов по JS'}
+      />
+      <div
+        ref={lastElement}
+        style={{height: '10px', background: 'red', marginBottom: '20px'}}
+      />
+      {isPostsLoading &&
+        <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}>
           <Loader/>
         </div>
-        : <PostList
-          remove={removePost}
-          posts={sortedAndSearchedPosts}
-          title={'Список постов по JS'}
-        />
       }
-
       <Pagination
         totalPages={totalPages}
         page={page}
